682f5ec6374589f2c637bbf9576b6c01745df617
Pedro-Afonso3 <paccj3@gmail.com>
2024-12-06 21:56:47 -0300

    fix: add anottation repository

--- src/main/java/com/app/App_projects/repository/usersRepository.java
+++ src/main/java/com/app/App_projects/repository/usersRepository.java
@@ -3,10 +3,12 @@ import com.app.App_projects.domain.participantes.Participantes;
 import com.app.App_projects.domain.users.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface usersRepository extends JpaRepository<Users,String> {
 
     UserDetails findByLogin(String login);
